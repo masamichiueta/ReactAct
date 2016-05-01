478a1018a7ae7ae90528ecb195ff31ffec53d16c
Masamichi Ueta <masamichi1023@gmail.com>
2016-05-01 09:51:40 +0900

    Finish tutorial5

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -7,20 +7,20 @@ export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: "Masamichi",
+      title: "ReactAct",
     };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
   }
 
   render() {
     return (
       <div>
-        <Header title={this.state.name}/>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer />
       </div>
-      // <div>
-      //
-      //   <Footer />
-      // </div>
     );
   }
 }
