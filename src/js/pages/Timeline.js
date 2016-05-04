819b3064aecd863d0a4e030291505023a11e5d18
Masamichi Ueta <masamichi1023@gmail.com>
2016-05-04 15:14:13 +0900

    Add timeline

---
+++ src/js/pages/Timeline.js
@@ -0,0 +1,56 @@
+import "./Timeline.scss";
+
+import React from "react";
+
+import Activity from "../components/Activity";
+import * as ActivityActions from "../actions/ActivityActions";
+import ActivityStore from "../stores/ActivityStore.js";
+
+export default class Timeline extends React.Component {
+
+  constructor() {
+    super();
+    this.getActivities = this.getActivities.bind(this);
+    this.state = {
+      activities: ActivityStore.getAll(),
+    };
+  }
+
+  componentWillMount() {
+    ActivityStore.on("change", this.getActivities);
+  }
+
+  componentDidMount() {
+    this.loadActivities();
+  }
+
+  componentWillUnmount() {
+    ActivityStore.removeListener("change", this.getActivities);
+  }
+
+  getActivities() {
+    this.setState({
+      activities: ActivityStore.getAll(),
+    });
+  }
+
+  loadActivities() {
+    ActivityActions.loadActivities();
+  }
+
+
+  render() {
+    const { activities } = this.state;
+    const ActivityComponents = activities.map((activity, i) => {
+      var inverted = i % 2 == 0 ? false : true;
+      return <Activity key={i} text={activity.text} imageUrl={activity.imageUrl} inverted={inverted} />
+    })
+
+    return (
+      <ul class="timeline">
+        {ActivityComponents}
+      </ul>
+    );
+  }
+
+}
---
+++ src/js/style.js
@@ -0,0 +1,6 @@
+export const colors = {
+  mainColor: "#FF79A6",
+  instagramColor: "#3f729b",
+  twitterColor: "#55acee",
+  facebookColor: "3b5998",
+};
