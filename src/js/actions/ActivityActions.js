5f7e8a4b6f3e9d92259a62862d53f8e97d29ccd4
micchyboy1023 <masamichi1023@gmail.com>
2016-05-11 23:10:45 +0900

    Add facebook graph api

--- src/js/actions/ActivityActions.js
+++ src/js/actions/ActivityActions.js
@@ -1,64 +1,91 @@
 import dispatcher from "../dispatcher";
 
-export function loadActivities() {
-
+function getInstagramActivities() {
   const instagramEndpoint = "https://api.instagram.com/v1/users/self/media/recent";
   const instagramAccessToken = "16866771.e0000c4.2ad588423c3d46068e87b5e5d0959340";
 
+  let defer = $.Deferred();
+  $.ajax({
+    url: instagramEndpoint,
+    data: { access_token: instagramAccessToken },
+    dataType: "jsonp"
+  }).done(function(data, textStatus, jqXHR) {
+    let instagramActivities = data.data.map((data) => {
+      const date = new Date(parseInt(data.created_time) * 1000);
+
+      return {
+        text: data.caption.text,
+        link: data.link,
+        imageUrl: data.images.thumbnail.url,
+        date: date
+      }
+    });
+    defer.resolve(instagramActivities);
+  }).fail(function() {
+    defer.reject();
+  });
+
+  return defer.promise();
+};
+
+function getGitHubActivities() {
   const gitHubEndpoint = "https://api.github.com/users/micchyboy1023/events";
 
-  let getInstagramActivities = function() {
-    let defer = $.Deferred();
-    $.ajax({
-      url: instagramEndpoint,
-      data: { access_token: instagramAccessToken },
-      dataType: "jsonp"
-    }).done(function(data, textStatus, jqXHR) {
-      let instagramActivities = data.data.map((data) => {
-        const date = new Date(parseInt(data.created_time) * 1000);
-
-        return {
-          text: data.caption.text,
-          link: data.link,
-          imageUrl: data.images.thumbnail.url,
-          date: date
-        }
-      });
-      defer.resolve(instagramActivities);
-    }).fail(function() {
-      defer.reject();
+  let defer = $.Deferred();
+  $.ajax({
+    url: gitHubEndpoint,
+    dataType: "jsonp"
+  }).done(function(data, textStatus, jqXHR) {
+    let gitHubActivities = data.data.map((data) => {
+      return {
+        text: data.type,
+        link: data.repo.url,
+        imageUrl: null,
+        date: new Date(data.created_at)
+      }
     });
+    defer.resolve(gitHubActivities);
+  }).fail(function() {
+    defer.reject();
+  });
 
-    return defer.promise();
-  };
+  return defer.promise();
+};
 
-  let getGitHubActivities = function() {
-    let defer = $.Deferred();
-    $.ajax({
-      url: gitHubEndpoint,
-      dataType: "jsonp"
-    }).done(function(data, textStatus, jqXHR) {
-      let gitHubActivities = data.data.map((data) => {
-        return {
-          text: data.type,
-          link: data.repo.url,
-          imageUrl: data.actor.avatar_url,
-          date: new Date(data.created_at)
-        }
-      });
-      defer.resolve(gitHubActivities);
-    }).fail(function() {
-      defer.reject();
+function getFacebookActivities() {
+  const facebookEndpoint = "https://graph.facebook.com/v2.6/me/posts";
+  const facebookAccessToken = "EAAHxpxZA3Ug0BADliqqRrsJExqRZB0BPhVWPdLtwbccylYXbCkx04N6L9CIL9iVECTR7e1FObDIpO1alX8x6fMhBIa7uZAc0d9lmjPw4iJmgb9JEQYq8MQv8tWND3nvIQW8vBgPMuFYxh5Ua1wa8VlZAK8RzmW27SybiplTVywZDZD";
+  let defer = $.Deferred();
+  $.ajax({
+    url: facebookEndpoint,
+    data: { access_token: facebookAccessToken },
+    dataType: "jsonp"
+  }).done(function(data, textStatus, jqXHR) {
+    console.log(data);
+    let facebookActivities = data.data.map((data) => {
+      return {
+        text: data.story,
+        link: 'https://facebook.com',
+        imageUrl: null,
+        date: new Date(data.created_time)
+      }
     });
+    defer.resolve(facebookActivities);
+  }).fail(function() {
+    defer.reject();
+  });
 
-    return defer.promise();
-  };
+  return defer.promise();
+}
+
+export function loadActivities() {
 
   $.when(
     getInstagramActivities(),
-    getGitHubActivities()
-  ).done(function(data1, data2) {
-    let activities = data1.concat(data2).sort((a, b) => {
+    getGitHubActivities(),
+    getFacebookActivities()
+  ).done(function(data1, data2, data3) {
+    let activities = data1.concat(data2).concat(data3).sort((a, b) => {
       return b.date - a.date;
     });
     dispatcher.dispatch({type: "RECEIVE_ACTIVITIES", activities: activities});
